707abccc6f2f98fb975a1337f1e70b2a44580c2b
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-22 06:24:00 +0530

    migration to apollo middleware

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -4,6 +4,10 @@ import { users } from '../dummyData/data.js';
 const userResolver = {
 	Query: {
 		users: () => users,
+        // 4 things we get inorder: parent, args, context, info
+        // parent no need now so _
+        //  args we destructured and get userId
+        user: (_, { userId }) => users.find((user) => user._id === userId),
 	},
 	Mutation: {},
 };
