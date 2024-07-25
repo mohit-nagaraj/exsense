306138613da804a0dfa6e8b37f9d147ad87f4bce
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-25 19:59:00 +0530

    header ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,21 @@ import Loginpage from "./pages/LoginPage.jsx";
 import SignUppage from "./pages/SignUpPage.jsx";
 import TransactionPage from "./pages/TransactionPage.jsx";
 import NotFoundpage from "./pages/NotFoundPage.jsx";
+import Header from "./components/ui/Header.jsx";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Homepage />} />
-				<Route path='/login' element={<Loginpage />} />
-				<Route path='/signup' element={<SignUppage />} />
-				<Route path='/transaction/:id' element={<TransactionPage />} />
-				<Route path='*' element={<NotFoundpage />} />
-			</Routes>
-		</>
-	);
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/signup" element={<SignUppage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundpage />} />
+      </Routes>
+    </>
+  );
 }
 export default App;
